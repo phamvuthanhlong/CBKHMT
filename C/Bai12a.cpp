ab718bc5b51670a49b771719a454b267dc49ce03
kkrot <84622322+phamvuthanhlong@users.noreply.github.com>
2022-07-19 21:15:13 +0700

    Update Bai12a.cpp

--- C/Bai12a.cpp
+++ C/Bai12a.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 /* Bai 12: Khai bao *a, *b, *c, x, y. In ra gia tri x, y. */
 
-int *a, *b, *c, x, y;
+double *a, *b, *c, x, y;
 void calc(){
-    x = (*a+*b+*c)/3;
-    y = (pow(*a-x,2)+pow(*b-x,2)+pow(*c-x,2))/3;
+    x = (*a + *b + *c)/3;
+    y = (pow(*a-x,2) + pow(*b-x,2) + pow(*c-x,2))/3;
 }
 main(){
-    scanf("%d %d %d", a, b, c);
-    printf("x = %d, y = %d", *a, *b);
+	double pa, pb, pc;
+    scanf("%lf %lf %lf", &pa, &pb, &pc);
+    a = &pa; b = &pb; c = &pc;
     calc();
-    printf("x = %d, y = %d", x, y);
+    printf("x = %lf, y = %lf\n", x, y);
 }
